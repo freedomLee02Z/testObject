290e686e6cee63fcc8b4c0158b4986f0ff57c119
lipan01@mucang.cn <lipan>
2021-10-27 19:11:56 +0800

    修改

--- src/main/java/com/example/demo/controller/UtilClass.java
+++ src/main/java/com/example/demo/controller/UtilClass.java
@@ -1,8 +1,5 @@
 package com.example.demo.controller;
 
-
-import com.alibaba.fastjson.JSONObject;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
