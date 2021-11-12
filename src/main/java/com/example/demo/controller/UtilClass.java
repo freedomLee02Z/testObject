b21b954ce97e6480bfc229b90fd3fc01334581db
lipan01@mucang.cn <lipan>
2021-11-12 18:07:38 +0800

    test

--- src/main/java/com/example/demo/controller/UtilClass.java
+++ src/main/java/com/example/demo/controller/UtilClass.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -9,23 +11,25 @@ import java.util.Map;
 
 @RestController
 public class UtilClass {
-    @RequestMapping("/getR")
-    public static  Map getR(){
-        Double num = (double)(new Date()).getTime() * Math.random() * 10000.0D;
+    @PostMapping("/getR")
+    public static Map getR(@RequestBody JSONObject data) {
+        Double num = (double) (new Date()).getTime() * Math.random() * 10000.0D;
         Long parseNum = (new Double(num)).longValue();
         Long val = Math.abs(parseNum);
         String key = val + "";
         int sum = 0;
 
-        for(int i = 0; i < key.length(); ++i) {
+        for (int i = 0; i < key.length(); ++i) {
             sum += Integer.parseInt(String.valueOf(key.charAt(i)));
         }
 
         sum += key.length();
         String sums = String.format("%03d", sum);
-        String result = key + sums;
-        Map mapResult  = new HashMap<>();
-        mapResult.put("result",result);
+        String result = data.get("version") + key + sums;
+        Map mapResult = new HashMap<>();
+        mapResult.put("result", result);
+
+
         return mapResult;
     }
 }
