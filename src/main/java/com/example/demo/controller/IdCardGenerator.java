7af90f5eb4df3a4291497bf05c9b59094f9aba56
lipan01@mucang.cn <lipan>
2021-11-16 17:41:27 +0800

    修改返回值

--- src/main/java/com/example/demo/controller/IdCardGenerator.java
+++ src/main/java/com/example/demo/controller/IdCardGenerator.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
